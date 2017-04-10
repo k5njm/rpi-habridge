99e23618f0aee8eae5fa8f1e46bc7b1e912f04a8
Nick McCarthy <nick@nickmcc.com>
2017-04-10 04:08:52 +0000

    updated device.db

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ docker stop habridge
 docker rm habridge
 docker pull kd5ftn/docker-ha-bridge
 #docker run -v /home/nick/habridge:/root/habridge/data/:rw -v /etc/localtime:/etc/localtime -p 80:80 --name habridge --restart=always -d kd5ftn/docker-ha-bridge:latest
-#docker run -v /home/nick/habridge:/root/habridge/data/:rw -v /etc/localtime:/etc/localtime --net=host --name habridge --restart=always -d kd5ftn/docker-ha-bridge:latest
-docker run -v /home/nick/habridge:/data/:rw -v /etc/localtime:/etc/localtime -p 80:80 -p 50000:50000 --name habridge --restart=always -d kd5ftn/docker-ha-bridge:latest
+docker run -v /home/nick/habridge:/data/:rw -v /etc/localtime:/etc/localtime --net=host --name habridge --restart=always -d kd5ftn/docker-ha-bridge:latest
+#docker run -v /home/nick/habridge:/data/:rw -v /etc/localtime:/etc/localtime -p 80:80 -p 50000:50000 --name habridge --restart=always -d kd5ftn/docker-ha-bridge:latest
 docker ps
